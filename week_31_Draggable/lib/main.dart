387e64c3b2cc2251a800451407b881a922250729
Charles Cai <caicongchen@gmail.com>
2020-06-10 22:57:09 -0400

    add practice code for week 31 Draggable

--- week_31_Draggable/lib/main.dart
+++ week_31_Draggable/lib/main.dart
@@ -49,6 +49,43 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'Dragglable Demo Is here ',
             ),
+            Draggable<Color>(
+              child: Container(
+                width: 50,
+                height: 50,
+                color: Colors.black54,
+              ),
+              childWhenDragging: Container(
+                width: 50,
+                height: 50,
+                color: Colors.red,
+              ),
+              feedback: Container(
+                width: 50,
+                height: 50,
+                color: Colors.grey,
+              ),
+              data: Colors.black,
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            Text('Target is below'),
+            DragTarget<Color>(
+              builder: (context, candiates, rejects) {
+                return candiates.length > 0
+                    ? Container(
+                        width: 100,
+                        height: 100,
+                        color: candiates[0],
+                      )
+                    : Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.grey,
+                      );
+              },
+            )
           ],
         ),
       ),

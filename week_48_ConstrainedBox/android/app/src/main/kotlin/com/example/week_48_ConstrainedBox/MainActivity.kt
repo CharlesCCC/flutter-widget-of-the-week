bfbab473c8b47e1ec5e9bb9accc9f32b5be8caa6
Charles Cai <caicongchen@gmail.com>
2020-06-27 23:00:51 -0400

    initial project for week 48

---
+++ week_48_ConstrainedBox/android/app/src/main/kotlin/com/example/week_48_ConstrainedBox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.week_48_ConstrainedBox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ week_48_ConstrainedBox/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo ConstrainedBox',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo ConstrainedBox Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'This is a nomrla Text without any constraint ',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

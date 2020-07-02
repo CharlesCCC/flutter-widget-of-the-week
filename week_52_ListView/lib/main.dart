702ddced04269fc89db7cb8764063fba1ba140ff
Charles Cai <caicongchen@gmail.com>
2020-07-01 23:37:38 -0400

    add practice code for week 52 ListView

--- week_52_ListView/lib/main.dart
+++ week_52_ListView/lib/main.dart
@@ -28,7 +28,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> _list = ['a', 'b', 'c', 'd'];
+  final List<String> _list = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+  ];
+  final List<int> colorCodes = <int>[600, 500, 100];
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +53,43 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Fixed List',
+            ),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    color: Colors.amber[600],
+                    child: const Center(child: Text('Entry A')),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.amber[500],
+                    child: const Center(child: Text('Entry B')),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.amber[100],
+                    child: const Center(child: Text('Entry C')),
+                  ),
+                ],
+              ),
+            ),
+            Text('Scrollable Dynamic List'),
+            Expanded(
+              child: ListView.builder(
+                  padding: const EdgeInsets.all(15),
+                  itemCount: _list.length,
+                  itemBuilder: (ctx, index) {
+                    return Container(
+                      height: 75,
+                      margin: EdgeInsets.all(15),
+                      color: Colors.amber[colorCodes[index % 2]],
+                      child: Text(_list[index]),
+                    );
+                  }),
             ),
           ],
         ),

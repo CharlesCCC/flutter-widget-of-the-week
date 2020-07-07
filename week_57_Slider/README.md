5146fc0aba79d7258b53133f2bb4fa68a24abf91
Charles Cai <caicongchen@gmail.com>
2020-07-06 22:42:57 -0400

    add practice code for week 57 Slider

--- week_57_Slider/README.md
+++ week_57_Slider/README.md
@@ -9,3 +9,70 @@ A new Flutter project.
 - Official Doc: https://api.flutter.dev/flutter/material/Slider-class.html
   - https://api.flutter.dev/flutter/cupertino/CupertinoSlider-class.html
   - https://api.flutter.dev/flutter/material/RangeSlider-class.html
+
+```dart
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Normal Slider Value $_value',
+              style: TextStyle(fontSize: 35),
+            ),
+            Slider(
+              value: _value,
+              max: 5.0,
+              min: 0.0,
+              divisions: 4,
+              onChanged: (double value) {
+                setState(() {
+                  _value = value;
+                });
+              },
+            ),
+            Text(
+              'Range Slider Value $_rangeValues',
+              style: TextStyle(fontSize: 25),
+            ),
+            RangeSlider(
+              values: _rangeValues,
+              min: 0,
+              max: 5,
+              onChanged: (value) {
+                setState(() {
+                  _rangeValues = value;
+                });
+              },
+            ),
+            Text(
+              'Cupertino Slider Value $_cvalue',
+              style: TextStyle(fontSize: 35),
+            ),
+            CupertinoSlider(
+              value: _cvalue,
+              min: 0.0,
+              max: 5.0,
+              onChanged: (value) {
+                setState(() {
+                  _cvalue = value;
+                });
+              },
+            ),
+            Text(
+              'Adaptive Slider Value $_avalue',
+              style: TextStyle(fontSize: 35),
+            ),
+            Slider.adaptive(
+              min: 0.0,
+              max: 5.0,
+              value: _avalue,
+              onChanged: (value) {
+                setState(() {
+                  _avalue = value;
+                });
+              },
+            )
+          ],
+        ),
+      ),
+```
+
--- week_57_Slider/lib/main.dart
+++ week_57_Slider/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,13 +29,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _value = 0.0;
+  var _cvalue = 0.0;
+  var _avalue = 0.0;
+  RangeValues _rangeValues = RangeValues(1, 3);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Normal Slider Value $_value',
+              style: TextStyle(fontSize: 35),
+            ),
+            Slider(
+              value: _value,
+              max: 5.0,
+              min: 0.0,
+              divisions: 4,
+              onChanged: (double value) {
+                setState(() {
+                  _value = value;
+                });
+              },
+            ),
+            Text(
+              'Range Slider Value $_rangeValues',
+              style: TextStyle(fontSize: 25),
+            ),
+            RangeSlider(
+              values: _rangeValues,
+              min: 0,
+              max: 5,
+              onChanged: (value) {
+                setState(() {
+                  _rangeValues = value;
+                });
+              },
+            ),
+            Text(
+              'Cupertino Slider Value $_cvalue',
+              style: TextStyle(fontSize: 35),
+            ),
+            CupertinoSlider(
+              value: _cvalue,
+              min: 0.0,
+              max: 5.0,
+              onChanged: (value) {
+                setState(() {
+                  _cvalue = value;
+                });
+              },
+            ),
+            Text(
+              'Adaptive Slider Value $_avalue',
+              style: TextStyle(fontSize: 35),
+            ),
+            Slider.adaptive(
+              min: 0.0,
+              max: 5.0,
+              value: _avalue,
+              onChanged: (value) {
+                setState(() {
+                  _avalue = value;
+                });
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }

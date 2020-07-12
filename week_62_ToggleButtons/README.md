216f4bc868b62bc7324de763e2b3cebbd80a15f7
Charles Cai <caicongchen@gmail.com>
2020-07-11 22:49:00 -0400

    add practice code for week 62 ToggleButtons

--- week_62_ToggleButtons/README.md
+++ week_62_ToggleButtons/README.md
@@ -7,3 +7,72 @@ A new Flutter project.
 - Do you want to let your users select from a number of related options? Adding ToggleButtons to your app can help!  
 - Official Youtube Tutorial: https://youtu.be/kVEguaQWGAY
 - Official Doc: https://api.flutter.dev/flutter/material/ToggleButtons-class.html
+
+```dart
+body: Column(
+        children: <Widget>[
+          Text('ToggleButtons#1: Combo '),
+          Center(
+            child: ToggleButtons(
+              children: <Widget>[
+                Icon(Icons.ac_unit),
+                Icon(Icons.call),
+                Icon(Icons.cake),
+              ],
+              onPressed: (int index) {
+                setState(() {
+                  print(isSelected.length);
+                  isSelected[index] = !isSelected[index];
+                });
+              },
+              isSelected: isSelected,
+            ),
+          ),
+          Text('ToggleButtons#1: Mutual Exclusive '),
+          ToggleButtons(
+            children: <Widget>[
+              Icon(Icons.ac_unit),
+              Icon(Icons.call),
+              Icon(Icons.cake),
+            ],
+            onPressed: (int index) {
+              setState(() {
+                for (int buttonIndex = 0;
+                    buttonIndex < isSelected.length;
+                    buttonIndex++) {
+                  if (buttonIndex == index) {
+                    isSelected[buttonIndex] = true;
+                  } else {
+                    isSelected[buttonIndex] = false;
+                  }
+                }
+              });
+            },
+            isSelected: isSelected,
+          ),
+          Text('ToggleButtons#3: Mutual Exclusive with Null Allowed '),
+          ToggleButtons(
+            children: <Widget>[
+              Icon(Icons.ac_unit),
+              Icon(Icons.call),
+              Icon(Icons.cake),
+            ],
+            onPressed: (int index) {
+              setState(() {
+                for (int buttonIndex = 0;
+                    buttonIndex < isSelected.length;
+                    buttonIndex++) {
+                  if (buttonIndex == index) {
+                    isSelected[buttonIndex] = !isSelected[buttonIndex];
+                  } else {
+                    isSelected[buttonIndex] = false;
+                  }
+                }
+              });
+            },
+            isSelected: isSelected,
+          ),
+        ],
+      ),
+```
+
--- week_62_ToggleButtons/lib/main.dart
+++ week_62_ToggleButtons/lib/main.dart
@@ -28,13 +28,79 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<bool> isSelected = [false, false, false];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: Column(
+        children: <Widget>[
+          Text('ToggleButtons#1: Combo '),
+          Center(
+            child: ToggleButtons(
+              children: <Widget>[
+                Icon(Icons.ac_unit),
+                Icon(Icons.call),
+                Icon(Icons.cake),
+              ],
+              onPressed: (int index) {
+                setState(() {
+                  print(isSelected.length);
+                  isSelected[index] = !isSelected[index];
+                });
+              },
+              isSelected: isSelected,
+            ),
+          ),
+          Text('ToggleButtons#1: Mutual Exclusive '),
+          ToggleButtons(
+            children: <Widget>[
+              Icon(Icons.ac_unit),
+              Icon(Icons.call),
+              Icon(Icons.cake),
+            ],
+            onPressed: (int index) {
+              setState(() {
+                for (int buttonIndex = 0;
+                    buttonIndex < isSelected.length;
+                    buttonIndex++) {
+                  if (buttonIndex == index) {
+                    isSelected[buttonIndex] = true;
+                  } else {
+                    isSelected[buttonIndex] = false;
+                  }
+                }
+              });
+            },
+            isSelected: isSelected,
+          ),
+          Text('ToggleButtons#1: Mutual Exclusive with Null Allowed '),
+          ToggleButtons(
+            children: <Widget>[
+              Icon(Icons.ac_unit),
+              Icon(Icons.call),
+              Icon(Icons.cake),
+            ],
+            onPressed: (int index) {
+              setState(() {
+                for (int buttonIndex = 0;
+                    buttonIndex < isSelected.length;
+                    buttonIndex++) {
+                  if (buttonIndex == index) {
+                    isSelected[buttonIndex] = !isSelected[buttonIndex];
+                  } else {
+                    isSelected[buttonIndex] = false;
+                  }
+                }
+              });
+            },
+            isSelected: isSelected,
+          ),
+        ],
+      ),
     );
   }
 }

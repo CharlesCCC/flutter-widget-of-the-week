1c27902e37f2df90853ce64f4fcc5d9a8eb7337f
Charles Cai <caicongchen@gmail.com>
2020-07-28 22:54:03 -0400

    add practice code for week 79 AnimatedWidget

--- week_79_AnimatedWidget/README.md
+++ week_79_AnimatedWidget/README.md
@@ -8,3 +8,59 @@ A new Flutter project.
 - Official Youtube Tutorial: https://youtu.be/LKKgYpC-EPQ
 - Official Doc: https://api.flutter.dev/flutter/widgets/AnimatedWidget-class.html
 
+```dart
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SpinningContainer(controller: _controller),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Stop'),
+        onPressed: () {
+          setState(() {
+            _controller.stop();
+          });
+        },
+      ),
+    );
+  }
+}
+
+class SpinningContainer extends AnimatedWidget {
+  const SpinningContainer({Key key, AnimationController controller})
+      : super(key: key, listenable: controller);
+
+  Animation<double> get _progress => listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: _progress.value * 2.0 * math.pi,
+      child: Container(width: 200.0, height: 200.0, color: Colors.blue),
+    );
+  }
+}
+```
+
--- week_79_AnimatedWidget/lib/main.dart
+++ week_79_AnimatedWidget/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(MyApp());
@@ -27,14 +28,56 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: Center(
+        child: SpinningContainer(controller: _controller),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Stop'),
+        onPressed: () {
+          setState(() {
+            _controller.stop();
+          });
+        },
+      ),
+    );
+  }
+}
+
+class SpinningContainer extends AnimatedWidget {
+  const SpinningContainer({Key key, AnimationController controller})
+      : super(key: key, listenable: controller);
+
+  Animation<double> get _progress => listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: _progress.value * 2.0 * math.pi,
+      child: Container(width: 200.0, height: 200.0, color: Colors.blue),
     );
   }
 }
